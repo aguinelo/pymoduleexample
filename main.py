45027603a92bfe8d5468f2a03e5eab03022a014a
Aguinelo Koczkodai <aguinelo@gmail.com>
2017-11-22 22:46:38 -0200

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from modules.module1.module1 import f1
+from modules.module1.module1 import Class1
+
+def oi():
+    print('oi')
+
+oi()
+
+f1()
+
+o1 = Class1()
+o1.m1()
---
+++ modules/module1/module1.py
@@ -0,0 +1,6 @@
+def f1():
+    print('f1')
+
+class Class1:
+    def m1(self):
+        print('m1 da Class 1')
